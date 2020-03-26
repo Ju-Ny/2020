a628011736c64b6bb56e7c4818b400ba730a0767
Jukka Nyyssönen <50370696+Ju-Ny@users.noreply.github.com>
2020-03-26 09:13:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const app = express();
+
+const fs=require("fs");
+
+const xml2js = require("xml2js");
+
+const jwt = require("restify-jwt-community");
+
+const bodyParser = require("body-parser");
+
+const kayttajat = require("./models/kayttajat");
+
+const rss = require("./models/rss");
+
+const portti = 3113;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ "extended" : true}));
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
+
+    app.get("/", (req, res) => {
+
+    kayttajat.haeTreenit((err, data) => {
+
+        if (err) throw err;
+
+    res.render("index" , {"kayttajat" : data});
+    
+
+        })
+
+    });
+
+    app.get("/aloita/", (req, res) => {
+
+    kayttajat.haeTreenit((err, data) => {
+
+        if (err) throw err;
+
+    res.render("aloita" , {"aloita" : []});
+
+        })
+
+    });
+
+
+    app.post("/tallenna/", (req, res) => {
+
+        kayttajat.tallennaTreenit(req.body, (err) => {
+
+            if (err) throw err;
+
+            res.redirect("/");;
+
+        }) 
+
+    });  
+
+    /*app.post("/kirjaudu/", (req, res) => {
+
+                let nimi = req.body.nimi;
+                let salasana = req.body.salasana;
+                
+                if (nimi != "" && salasana !="")     {
+
+                    res.send("Oikein")
+
+                    res.redirect("aloita")
+    
+                }
+
+                else     {
+                        console.log("Skulaa");
+                    res.redirect("/kirjaudu")
+     
+                 }     
+
+    });*/
+
+    app.get("/kaikki", (req, res) => {
+        
+     rss.uudetUutiset((virhe, uudet) => {  
+         
+        if (uudet.length > 5) {
+
+            uudet.length = 5;
+
+        }
+
+        res.render("kaikki", {"uudet" : uudet, "virhe" : virhe});
+
+    });
+    
+    });
+
+app.listen(portti, () => {
+
+    console.log(`Palvelin käynnistyi porttiin: ${portti}`);
+
+});
